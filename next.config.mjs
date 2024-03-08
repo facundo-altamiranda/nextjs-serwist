a62b81f7c5e72997fb97c526df376c8fa2c9d297
solvd <solvd@192.168.1.19>
2024-03-08 09:35:26 -0300

    First commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withSerwistInit from "@serwist/next";
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  swcMinify: true,
+  reactStrictMode: true
+};
+
+const withSerwist = withSerwistInit({
+  cacheOnFrontEndNav: true,
+  swSrc: "sw.ts",
+  swDest: "public/sw.js",
+  reloadOnOnline: true,
+  // disable: process.env.NODE_ENV === "development",
+});
+
+export default withSerwist(nextConfig);
